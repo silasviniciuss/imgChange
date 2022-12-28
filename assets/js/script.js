e6ab2644c0fab1688974d267bb94aa136c3eb14c
silasviniciuss <silasvinicius.dev@gmail.com>
2022-12-28 02:10:12 -0300

    Projeto concluido

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var number_img = 1;
+function img_changer_sub(){ 
+    var img = document.getElementById("img");
+
+    if(number_img > 1){
+        number_img -= 1;
+    } else{
+        number_img = 1;
+    }    
+
+    img.src = "assets/img/img"+ number_img +".png";
+
+}
+
+function img_changer_add(){    
+    var img = document.getElementById("img");
+
+    if(number_img < 3){
+        number_img += 1;
+    } else{
+        number_img = 3
+    }    
+
+    img.src = "assets/img/img"+ number_img +".png";
+
+}
